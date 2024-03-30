fc81d727100ada40a6009d10feb61b4bb2732580
devasuryapradeep <devasuryapradeep4@gmail.com>
2024-03-30 17:54:14 +0530

    few updates

--- HackerNews/ListView.swift
+++ HackerNews/ListView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ListView: View {
+    
     /// Variable declarations
     @ObservedObject  var APIManager = NetworkManger()
     @State var screenTransitionVariable: Bool = false
@@ -15,29 +16,29 @@ struct ListView: View {
         NavigationStack{
             VStack {
                 List(APIManager.postdata) {post in
-                        NavigationLink(value: post.url) {
-                            HStack{
-                                Text("\(post.points)")
-                                Spacer()
-                                Text(post.title)
-                                    .font(.headline)
-                                Spacer()
-                            }
-                            .navigationDestination(for: String.self) { value in
-                                DetailView(url: value)
-                            }
+                    NavigationLink(value: post.url) {
+                        HStack{
+                            Text("\(post.points)")
+                            Spacer()
+                            Text(post.title)
+                                .font(.headline)
+                            Spacer()
+                        }
+                        .navigationDestination(for: String.self) { value in
+                            DetailView(url: value)
                         }
                     }
-                    .listStyle(.insetGrouped)
                 }
-                .onAppear{
-                    APIManager.fetchData()
-                }
-                .navigationTitle("HackerNews")
-                .navigationBarTitleDisplayMode(.inline)
+                .listStyle(.insetGrouped)
             }
+            .onAppear{
+                APIManager.fetchData()
+            }
+            .navigationTitle("HackerNews")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
+}
 
 
 #Preview {
